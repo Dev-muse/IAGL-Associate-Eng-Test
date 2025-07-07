ce62c750f44dec8e21fa62879240f92594d3a9e0
Rahman <museabdirahman@gmail.com>
2025-07-07 16:59:39 +0100

    feat: complete backend with CRUD operations and tests

--- backend/test/repository/todo.test.js
+++ backend/test/repository/todo.test.js
@@ -1,14 +1,19 @@
-
 const repository = require('../../src/repository/todo');
 
 describe('TODO repository', () => {
-  it('should return the todo list', async () => {
-    const expected = {
-      todos: [{
-        "task": "This is a todo example"
-      }]
-    };
+  it('should return the todo list with a default task', async () => {
     const actual = await repository.getTodos();
-    expect(actual).toEqual(expected);
+
+    expect(Array.isArray(actual.todos)).toBe(true);
+    expect(actual.todos.length).toBeGreaterThan(0);
+
+    const firstTodo = actual.todos[0];
+
+    expect(firstTodo).toMatchObject({
+      task: "This is a todo example",
+      completed: false,
+    });
+
+    expect(typeof firstTodo.id).toBe('string');
   });
 });
